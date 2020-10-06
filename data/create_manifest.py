825f8f38e01f95c8523ff1ceaacc44363d6b4302
Aman Tiwari <amant5186@gmail.com>
2020-10-06 16:06:16 +0530

    Aman | Flexibility in create manifest

--- data/create_manifest.py
+++ data/create_manifest.py
@@ -1,17 +1,31 @@
 import os
 from pathlib import Path
 
-if os.path.isfile("data.csv"):
-    os.remove("data.csv")
+train_file_name = "train_data.csv"
+valid_file_name = "valid_data.csv"
+
+if os.path.isfile(train_file_name) or os.path.isfile(valid_file_name):
+    try:
+        os.remove(train_file_name)
+    except:
+        pass
+    try:
+        os.remove(valid_file_name)
+    except:
+        pass
 
 
-def create_manifest(path, label, ext):
+def create_manifest(path, label, ext, mode):
     audio_path = list(Path(path).glob("**/*." + ext))
-    file = open("data.csv", "a+")
+    if mode.lower() == "train":
+        file_name = train_file_name
+    else:
+        file_name = "valid_data.csv"
+    file = open(file_name, "a+", encoding="utf-8")
     for _ in audio_path:
         print(str(str(_) + "," + str(label)), file=file)
 
 
 if __name__ == "__main__":
-    create_manifest("/home/jupyter/lid_data/hindi", 0, "wav")
-    create_manifest("/home/jupyter/lid_data/english", 1, "wav")
+    create_manifest(path="/home/jupyter/lid_data/hindi", label=0, ext="wav", mode="Train")
+    create_manifest(path="/home/jupyter/lid_data/english", label=1, ext="wav", mode="Train")
