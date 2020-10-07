ed8f4ecb148484a642c1947a31f2ae731e8b7dba
Aman Tiwari <amant5186@gmail.com>
2020-10-07 16:27:18 +0530

    Aman | Misc changes in wav coversion

--- data/convert_to_wav.py
+++ data/convert_to_wav.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm
 from pathlib import Path
 
 
@@ -7,16 +8,14 @@ def convert(target_path, new_path, is_clean, ext):
         audio_path = list(Path(target_path).glob("**/clean/*." + ext))
     else:
         audio_path = list(Path(target_path).glob("**/*." + ext))
-    for file_path in audio_path:
+    for file_path in tqdm(audio_path):
         new_file_path = os.path.join(new_path, file_path.parent.stem, file_path.stem + ".wav")
         try:
             os.makedirs(os.path.join(new_path, file_path.parent.stem))
         except:
             pass
         cmd = "ffmpeg -i " + str(file_path) + " -ar 16000 -ac 1 -bits_per_raw_sample 16 -vn " + str(new_file_path)
-        if not os.system(cmd):
-            print("Encountered some error")
-            exit(1)
+        os.system(cmd)
 
 
 if __name__ == "__main__":
