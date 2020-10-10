4b7bb4df1b02a32cea01ed9449d86a8a35e23282
Aman Tiwari <amant5186@gmail.com>
2020-10-10 10:05:47 +0530

    Resnet pretrained set to true

--- models/model.py
+++ models/model.py
@@ -6,7 +6,7 @@ from torchvision.models import resnet18
 
 
 def get_model(device):
-    model = resnet18(pretrained=False)
+    model = resnet18(pretrained=True)
     model.fc = nn.Linear(512, 3)
     model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
     model.to(device, dtype=torch.float)
