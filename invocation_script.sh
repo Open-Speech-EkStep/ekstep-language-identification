2ade1a2636fec58e474f21eea048341cf4c7c7b5
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2020-12-15 12:09:18 +0530

    379 | Soujyo | Add tracking uri as argument

--- invocation_script.sh
+++ invocation_script.sh
@@ -3,8 +3,9 @@
 echo `pwd`
 echo $1
 echo $2
+echo $3
 #create manifests
 python3 data/create_manifest.py $1 $2
 #Start model train
-nohup python3 train.py &
+nohup python3 train.py $3 &
 #sleep 1000
