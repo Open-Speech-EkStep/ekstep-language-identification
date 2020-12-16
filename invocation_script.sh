503d167a8b45c065f841fe6702c62c5fca28bcca
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2020-12-16 14:41:22 +0530

    mlops | Soujyo & AMAN| Add validation script for LID to work with mlflow

--- invocation_script.sh
+++ invocation_script.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
-echo `pwd`
-echo $1
-echo $2
-echo $3
-#create manifests
-python3 data/create_manifest.py $1 $2
-#Start model train
-nohup python3 train.py $3 &
+echo $(pwd)
+train_set=$1
+validation_set=$2
+tracking_ui=$3
+mode=$4
+echo $train_set
+echo $validation_set
+echo $tracking_ui
+
+
 #sleep 1000
+
+if [ $mode = "train" ]; then
+  echo "The mode is training...."
+  #create manifests
+  python3 data/create_manifest.py $train_set $validation_set
+  #Start model train
+  nohup python3 train.py $tracking_ui &
+elif [ $mode = "validation" ]; then
+  echo "The mode is validation...."
+  python3 batch_validation.py $tracking_ui $validation_set
+elif [ $mode = "acceptance" ]; then
+  echo "The mode is acceptance...."
+elif [ $mode = "staging" ]; then
+  echo "The mode is promote to staging...."
+else
+  echo "Invalid mode..."
+fi
