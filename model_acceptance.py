6640bc5fc8847ab675aed27004f90a1658f6c67d
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2020-12-16 18:09:47 +0530

    mlops | Soujyo & AMAN| Add model acceptance

---
+++ model_acceptance.py
@@ -0,0 +1,33 @@
+import mlflow.pyfunc
+from mlflow.tracking import MlflowClient
+from utils.training import *
+import sys
+
+accuracy_score_threshold = sys.argv[1]
+valid_parameters = load_yaml_file("train_config.yml")["train_parameters"]
+model_name = valid_parameters["model_name"]
+
+client = MlflowClient()
+
+
+def load_model():
+    for mv in client.search_model_versions(f"name='{model_name}'"):
+        model_version = int(mv.version)
+
+    model = mlflow.pytorch.load_model(
+        model_uri=f"models:/{model_name}/{model_version}"
+    )
+    print(f"Model fetched with name : {model_name} and version {model_version}")
+    print(model)
+    return model
+
+
+model = load_model()
+
+run_id = model.metadata.run_id
+accuracy_metric = client.get_metric_history(run_id, "Accuracy")[-1].value
+
+if accuracy_metric>=accuracy_score_threshold:
+    print('Model accepted')
+else:
+    print('Model rejected')
