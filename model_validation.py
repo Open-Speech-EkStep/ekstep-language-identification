f09b364cb279a2b6fe310ebecf3a89f5d8f4aaac
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-05-26 22:21:02 +0530

    mlops | Soujyo & AMAN| Add config data to lid

--- model_validation.py
+++ model_validation.py
@@ -2,7 +2,7 @@ import sys
 
 from models.LIDModel import LIDModel
 
-config_path = sys.argv[0]
+config_path = sys.argv[1]
 lid_model = LIDModel(config_path=config_path + "/train_config.yml")
 
 lid_model.test()
