a2fef04196240e34f777cdc3a69165f2a8961bb9
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-05-26 19:23:30 +0530

    mlops | Soujyo & AMAN| Add training data to lid

--- invocation_script.sh
+++ invocation_script.sh
@@ -17,13 +17,13 @@ if [ $mode = "train" ]; then
   #create manifests
   python3 data/create_manifest.py $train_set $validation_set
   #Start model train
-  python3 train.py $tracking_ui
+  python3 model_training.py $tracking_ui
 elif [ $mode = "validation" ]; then
   echo "The mode is validation...."
   python3 model_validation.py $tracking_ui $validation_set
 elif [ $mode = "acceptance" ]; then
   echo "The mode is acceptance...."
-  python3 model_acceptance.py $tracking_ui .90
+  python3 model_acceptance.py $tracking_ui
 elif [ $mode = "staging" ]; then
   echo "The mode is promote to staging...."
 else
